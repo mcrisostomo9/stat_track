f06e3af5020c2d8f57b1ec3f35b95f6f99940b7c
robzdoom <jorgecruz.rd@gmail.com>
2017-04-11 13:01:08 -0700

    cards fixed

--- src/components/nba_scoreboard.js
+++ src/components/nba_scoreboard.js
@@ -10,30 +10,25 @@ class IndvidualGames extends React.Component{
              <div key = {index} className='card-deck'>
                <div className='card'>
                <div className='card-header'>Q4 3:01</div>
-                  <div>
-                   <div>{game.vTeam.triCode}</div>
-                    <div>23</div> 
-                      <h6>@</h6>
-                   <div> {game.hTeam.triCode} </div>
-                    <div> 26 </div>
+               <table className ='table'>
+                  <tbody>
+                    <tr>
+                      <ul>
+                        <td>{game.vTeam.triCode}</td>
+                            <td>23</td> 
+                      </ul>
+                      <ul>
+                          <td> {game.hTeam.triCode} </td>
+                          <td> 26 </td>
+                      </ul>    
+                    </tr>
+                    </tbody> 
                <div className='card-footer'>Staple Center</div>     
-                </div> 
+                </table>
                </div>   
              </div>
              
-                  /*tbody className='col-md-4'>
-              <tr>
-                <ul>        
-                  <td>{game.hTeam.triCode}</td>
-                  <td>120</td>
-                </ul>  
-               <ul>
-                  <td>{game.vTeam.triCode}</td>
-                  <td>92</td>
-               </ul>   
-              </tr>
-            </tbody>      */
-
+                
                
         )
       })

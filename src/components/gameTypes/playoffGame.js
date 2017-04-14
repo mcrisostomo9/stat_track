4c1602295a174df4fc09cf4ca0650b0baf15e040
Mark Crisostomo <mcrisostomo9@gmail.com>
2017-04-14 14:38:50 -0700

    added playoff format games, no game days need fixing still

---
+++ src/components/gameTypes/playoffGame.js
@@ -0,0 +1,83 @@
+import React from 'react';
+
+const PlayoffGame = (props)=> {
+  let {game} = props;
+  let {startTime} = props;
+  // for final score
+  if(game.attendance > 0 && game.clock === ""){
+  return(
+        <div  className='card-deck'>
+          <div className='card'>
+                <div className='card-header'> GAME {game.playoffs.gameNumInSeries}</div>
+                <table className='table'>
+                     <tbody>
+                       <tr>
+                         <ul>
+                           <td>{game.vTeam.triCode}</td>
+                           <td>{game.vTeam.score}</td>
+                         </ul>
+                         <ul>
+                             <td> {game.hTeam.triCode} </td>
+                             <td> {game.hTeam.score} </td>
+                         </ul>
+                       </tr>
+                    </tbody>
+                </table>
+                <div className='card-header'>{game.playoffs.seriesSummaryText}</div>
+          </div>
+        </div>
+    )
+  }
+  //for game that hasnt started
+  if(game.clock === ""){
+    return(
+      <div  className='card-deck'>
+        <div className='card'>
+              <div className='card-header'>GAME {game.playoffs.gameNumInSeries} Start time: {startTime}</div>
+              <table className='table'>
+                   <tbody>
+                     <tr>
+                       <ul>
+                         <td>{game.vTeam.triCode}</td>
+                         <td>{game.vTeam.score}</td>
+                       </ul>
+                       <ul>
+                           <td> {game.hTeam.triCode} </td>
+                           <td> {game.hTeam.score} </td>
+                       </ul>
+                     </tr>
+                  </tbody>
+              </table>
+              <div className='card-header'>{game.playoffs.seriesSummaryText}</div>
+        </div>
+      </div>
+  )
+}
+// for game in progress
+return(
+  <div  className='card-deck'>
+    <div className='card'>
+          <div className='card-header'>GAME {game.playoffs.gameNumInSeries} Clock: {game.clock}</div>
+          <table className='table'>
+               <tbody>
+                 <tr>
+                   <ul>
+                     <td>{game.vTeam.triCode}</td>
+                     <td>{game.vTeam.score}</td>
+                   </ul>
+                   <ul>
+                       <td> {game.hTeam.triCode} </td>
+                       <td> {game.hTeam.score} </td>
+                   </ul>
+                 </tr>
+              </tbody>
+          </table>
+            <div className='card-header'>{game.playoffs.seriesSummaryText}</div>
+    </div>
+  </div>
+)
+
+
+}
+
+export default PlayoffGame

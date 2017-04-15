4a74df24bd6db947e8908c27a6d9bfbd854389d8
Charles Kim <Charles Kim>
2017-04-14 18:04:09 -0700

    react-bootstrap added and header styled accordingly

--- src/components/header.js
+++ src/components/header.js
@@ -2,20 +2,36 @@
  * Created by ck111 on 4/13/2017.
  */
 import React from 'react';
-import {Link, IndexLink} from 'react-router';
+import {Nav, Navbar, NavItem} from 'react-bootstrap';
+import {LinkContainer, IndexLinkContainer} from 'react-router-bootstrap';
 
 export class Header extends React.Component{
     render(){
         return(
-            <nav className="navbar navbar-light bg-faded">
-                <div className="nav">
-                    <h1 className="navbar-brand mb-0">Logo</h1>
-                    <ul className="nav">
-                        <li className="nav-item"><IndexLink to="/" className="nav-link">Scoreboard</IndexLink></li>
-                        <li className="nav-item"><Link to="standings" className="nav-link">Standings</Link></li>
-                    </ul>
-                </div>
-            </nav>
+            <Navbar inverse collapseOnSelect>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <a href="/">LOGO</a>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav pullRight>
+                        <IndexLinkContainer to='/'><NavItem>Scoreboard</NavItem></IndexLinkContainer>
+                        <LinkContainer to='/standings' ><NavItem>Standings</NavItem></LinkContainer>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         )
     }
 }
+
+// <nav className="navbar navbar-light bg-faded">
+//     <div className="nav">
+//         <h1 className="navbar-brand mb-0">Logo</h1>
+//         <ul className="nav">
+//             <li className="nav-item"><IndexLinkContainer to="/" className="nav-link">Scoreboard</IndexLinkContainer></li>
+//             <li className="nav-item"><LinkContainer to="standings" className="nav-link">Standings</LinkContainer></li>
+//         </ul>
+//     </div>
+// </nav>

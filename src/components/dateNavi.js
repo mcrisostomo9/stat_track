1c37643cea90ec22832ccec31d7be4c98de168bb
mcrisostomo9 <mcrisostomo9@gmail.com>
2017-06-13 17:21:09 -0700

    disabled next and defaulted the date to last of the season

--- src/components/dateNavi.js
+++ src/components/dateNavi.js
@@ -7,8 +7,9 @@ import {Glyphicon} from "react-bootstrap";
 class DateNavi extends React.Component{
   constructor(props){
     super(props);
-    this.props.sendDate(new Date());
     this.props.dateFromApi();
+    //hardcoded the end of the season date
+    this.props.sendDate(new Date('Mon Jun 12 2017 17:14:22 GMT-0700 (PDT)'));
   }
   previousDay(){
     let temp = this.props.unformattedDate;
@@ -22,8 +23,7 @@ class DateNavi extends React.Component{
   }
 
   render(){
-      console.log('api date', this.props.endSeasonDate)
-
+      const { endSeasonDate, viewedDate } = this.props;
       return(
           <div>
               <div className="row">
@@ -34,7 +34,12 @@ class DateNavi extends React.Component{
                     <h4 className="date_longform">{this.props.viewedDateLongForm}</h4>
                   </div>
                   <div className="col-xs-4 text-left">
-                    <button className="btn btn-default" onClick={()=>this.nextDay()}>Next<Glyphicon glyph="chevron-right"/></button>
+                    {/*conditional to disable button for the end of the season*/}
+                    {
+                     (viewedDate < endSeasonDate) ?
+                         (<button className="btn btn-default" onClick={()=>this.nextDay()}>Next<Glyphicon glyph="chevron-right"/></button>):
+                         (<button className="btn btn-default disabled" disabled="disabled" onClick={()=>this.nextDay()}>Next<Glyphicon glyph="chevron-right"/></button>)
+                    }
                   </div>
               </div>
               <Scoreboard/>

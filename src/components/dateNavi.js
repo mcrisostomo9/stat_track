25308de3598d017ad6a4e1d14c2c9df4c145cab3
mcrisostomo9 <mcrisostomo9@gmail.com>
2017-07-22 21:22:27 -0700

    took off last date limiter

--- src/components/dateNavi.js
+++ src/components/dateNavi.js
@@ -8,8 +8,7 @@ class DateNavi extends React.Component{
   constructor(props){
     super(props);
     this.props.dateFromApi();
-    //hardcoded the end of the season date
-    this.props.sendDate(new Date('Mon Jun 12 2017 17:14:22 GMT-0700 (PDT)'));
+    this.props.sendDate(new Date());
   }
   previousDay(){
     let temp = this.props.unformattedDate;
@@ -23,7 +22,6 @@ class DateNavi extends React.Component{
   }
 
   render(){
-      const { endSeasonDate, viewedDate } = this.props;
       return(
           <div>
               <div className="row">
@@ -34,12 +32,7 @@ class DateNavi extends React.Component{
                     <h4 className="date_longform">{this.props.viewedDateLongForm}</h4>
                   </div>
                   <div className="col-xs-4 text-left">
-                    {/*conditional to disable button for the end of the season*/}
-                    {
-                     (viewedDate < endSeasonDate) ?
-                         (<button className="btn btn-default" onClick={()=>this.nextDay()}>Next<Glyphicon glyph="chevron-right"/></button>):
-                         (<button className="btn btn-default disabled" disabled="disabled" onClick={()=>this.nextDay()}>Next<Glyphicon glyph="chevron-right"/></button>)
-                    }
+                      <button className="btn btn-default" onClick={()=>this.nextDay()}>Next<Glyphicon glyph="chevron-right"/></button>
                   </div>
               </div>
               <Scoreboard/>

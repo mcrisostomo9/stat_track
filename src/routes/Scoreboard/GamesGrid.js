2eced3cdb67e511509d73d54465e6e32d6b67902
Andy Ong <aong93@gmail.com>
2017-08-22 09:45:52 -0700

    added some scoreboard background color

--- src/routes/Scoreboard/GamesGrid.js
+++ src/routes/Scoreboard/GamesGrid.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import {connect} from 'react-redux';
-import {fetchGamesFromApi} from '../../actions/actionCreators';
-import Game from '../../components/Games/Game'
-import styled from 'styled-components';
+import React from "react";
+import { connect } from "react-redux";
+import { fetchGamesFromApi } from "../../actions/actionCreators";
+import Game from "../../components/Games/Game";
+import styled from "styled-components";
 
 const GamesGridContainer = styled.div`
   margin-top: 3rem;
@@ -12,70 +12,62 @@ const GamesGridContainer = styled.div`
   grid-template-rows: 1fr;
 `;
 
-const LogoImage = styled.img`
-  width: 10rem;
-`;
-
+const LogoImage = styled.img`width: 10rem;`;
 
 class GamesGrid extends React.Component {
   //make call to fetch game on mount
   componentDidMount() {
-    let {viewedDate} = this.props;
+    let { viewedDate } = this.props;
     this.props.fetchGames(viewedDate);
   }
 
   //To handle the re-rendering when the viewedDate state changes
   componentWillReceiveProps(nextProps) {
-    let {viewedDate} = this.props;
-    if (viewedDate === nextProps.viewedDate)
-      return false;
+    let { viewedDate } = this.props;
+    if (viewedDate === nextProps.viewedDate) return false;
     this.props.fetchGames(nextProps.viewedDate);
     return true;
   }
 
   render() {
     //handles initial render to show loading of the games, probably need to refine tho
-    let {games} = this.props;
+    let { games } = this.props;
     if (!games) {
-      return (
-        <div>
-          loading...
-        </div>
-      )
+      return <div>loading...</div>;
     }
     if (games.length === 0 || this.props.noGames) {
       return (
         <GamesGridContainer>
           <div className="col-xs-6 col-xs-offset-3 text-center">
-            <img src="../assets/hoop_color_2.png" alt=""/>
+            <img src="../assets/hoop_color_2.png" alt="" />
             <h4>NO GAMES SCHEDULED</h4>
           </div>
         </GamesGridContainer>
-      )
+      );
     }
     return (
-        <GamesGridContainer>
-            <div>
-                <LogoImage src="../assets/hoop_color_2.png" alt=""/>
-            </div>
-            <Game/>
-        </GamesGridContainer>
-    )
+      <GamesGridContainer>
+        <div>
+          <LogoImage src="../assets/hoop_color_2.png" alt="" />
+        </div>
+        <Game />
+      </GamesGridContainer>
+    );
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
-          games: state.gamesData.games,
-          viewedDate: state.setDate.viewedDate,
-          noGames: state.gamesData.noGames
-         }
-}
+    games: state.gamesData.games,
+    viewedDate: state.setDate.viewedDate,
+    noGames: state.gamesData.noGames
+  };
+};
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
-    fetchGames: (day) => dispatch(fetchGamesFromApi(day))
-  }
-}
+    fetchGames: day => dispatch(fetchGamesFromApi(day))
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(GamesGrid);
--- src/routes/Scoreboard/Scoreboard.js
+++ src/routes/Scoreboard/Scoreboard.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import GamesGrid from './GamesGrid';
-import styled from 'styled-components';
-import DateNavi from './DateNavi';
+import React from "react";
+import GamesGrid from "./GamesGrid";
+import styled from "styled-components";
+import DateNavi from "./DateNavi";
 
 const Content = styled.div`
-  background-color: white;
+  background-color: #465e9b;
   margin: 3%;
   padding-top: 3%;
   border-radius: 1em;
 `;
 
-const Scoreboard = ()=>{
-    return(
-        <Content>
-            <DateNavi/>
-            <GamesGrid/>
-        </Content>
-    )
-}
+const Scoreboard = () => {
+  return (
+    <Content className="gameContainer">
+      <DateNavi />
+      <GamesGrid />
+    </Content>
+  );
+};
 
 export default Scoreboard;
